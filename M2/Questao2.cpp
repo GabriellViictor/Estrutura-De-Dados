e587acc0974322e9fce032c54d5df296574bb043
GabriellViictor <63075215+GabriellViictor@users.noreply.github.com>
2023-04-27 21:12:43 -0300

    Create Questao2.cpp

---
+++ M2/Questao2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string comando = " ";
+    float valor;
+    //Pilha expressao;
+    //expressao.cria;
+
+    cout << "Editor de expressao aritmetica" << endl;
+    cout << "digite 'HELP' para visualizar a lista de comandos disponiveis"<<endl;
+    do{
+       
+    }while(comando != "FIM");
+
+    return 0;
+}
